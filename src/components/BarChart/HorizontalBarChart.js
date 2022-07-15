f87a08a649bff51f074ffd1fe53b22f7120245d0
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-07-15 08:39:20 +0200

    Use selections with horizontal bar

--- src/components/BarChart/HorizontalBarChart.js
+++ src/components/BarChart/HorizontalBarChart.js
@@ -1,13 +1,19 @@
-import { max, scaleBand, scaleLinear } from 'd3';
-import { memo } from 'react';
+import { easeQuadOut, max, scaleBand, scaleLinear, select, transition } from 'd3';
+import { pipe, prop } from 'ramda';
+import { memo, useEffect, useRef } from 'react';
 import { numberWithCommas } from '../../utils';
 
 import XAxisNum from '../Axis/XAxisNum';
 import YAxisStr from '../Axis/YAxisStr';
 
-
+function getTransition () {
+  return transition()
+    .duration(1000)
+    .ease(easeQuadOut);
+}
 
 function HorizontalBarChart ({ data, xField, yField, width, height }) {
+  const svgRef = useRef();
   const margin = { top: 0, left: 100, bottom: 80, right: 0 };
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
@@ -21,8 +27,29 @@ function HorizontalBarChart ({ data, xField, yField, width, height }) {
     .range([0, innerHeight])
     .paddingInner(.15);
 
+  useEffect(() => {
+    const rects = select(svgRef.current)
+      .select('g')
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('height', yScale.bandwidth())
+      .attr('x', 0)
+      .attr('y', pipe(prop(yField), yScale))
+      .attr('fill', '#54BAB9');
+
+    rects.transition(getTransition())
+      .attr('width', pipe(prop(xField), xScale))
+      .delay(300);
+
+    rects.append('title')
+      .text(pipe(prop(xField), numberWithCommas));
+
+  }, []);
+
   return (
-    <svg width={width} height={height} >
+    <svg width={width} height={height} ref={svgRef}>
       <g transform={`translate(${margin.left},${margin.top})`}>
         <line
           y2={innerHeight}
@@ -41,21 +68,6 @@ function HorizontalBarChart ({ data, xField, yField, width, height }) {
           title={yField}
           yScale={yScale}
         />
-
-        {data.map(data => {
-          const barWidth = xScale(data[xField]);
-          return <rect
-            key={data[yField]}
-            width={barWidth}
-            height={yScale.bandwidth()}
-            x={0}
-            y={yScale(data[yField])}
-            fill='#54BAB9'
-            opacity={.8}
-          >
-            <title>{numberWithCommas(data[xField])}</title>
-          </rect>;
-        })}
       </g>
     </svg>
   );
